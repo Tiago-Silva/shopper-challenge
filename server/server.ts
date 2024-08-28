7198d7e0cd9934015565a0f593b5908f56822397
Tiago <ti.almeida58@gmail.com>
2024-08-28 20:56:51 -0300

    feat: configuration start application

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,11 @@
-console.log('Thank you for using WebStorm 💙')
+import express from 'express';
+import measureRoute from "./routes/measureRoute";
+
+const app = express();
+
+app.use(express.json());
+app.use('/',  measureRoute);
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
