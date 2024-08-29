204770ffdf4f5b26a3d0c66691e8177165afa5a0
Tiago <ti.almeida58@gmail.com>
2024-08-29 09:22:07 -0300

    feat: integration with GoogleGenerativeAI

--- server/routes/measureRoute.ts
+++ server/routes/measureRoute.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 import path from 'path';
+import dotenv from "dotenv";
+import {GoogleGenerativeAI} from "@google/generative-ai";
+
+dotenv.config();
 
 const router = express.Router();
+const configuration = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
+const modelId = 'gemini-1.5-flash';
+const model = configuration.getGenerativeModel({ model: modelId });
 
 router.post('/upload', async (req: Request, res: Response, next: NextFunction) => {
     const  { imageBase64, filename } = req.body;
@@ -18,9 +25,19 @@ router.post('/upload', async (req: Request, res: Response, next: NextFunction) =
     try {
         fs.writeFileSync(filePath, buffer);
 
+        const prompt = 'Extract the numerical reading from the following image, which shows a water/gas meter reading. The reading is in the form of a number, such as 12345';
+        const image = {
+            inlineData: {
+                data: base64Data,
+                mimeType: "image/png",
+            },
+        };
+
+        const result = await model.generateContent([prompt,image]);
+
         res.status(201).json({
            image_url: `/assets/${filename}`,
-           measure_value: '65',
+           measure_value: result.response.text(),
            measure_uuid: '9l2n3ekn3nio'
         });
     } catch (e) {
