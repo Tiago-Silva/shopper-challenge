61f5309925970c303c64813ade755903fd1ff1c6
Tiago <ti.almeida58@gmail.com>
2024-08-30 19:53:08 -0300

    refactor: creating DTOS

--- server/interface/interface.ts
+++ server/interface/interface.ts
@@ -16,16 +16,40 @@ export interface Customer {
     updatedAt?: Date;
 }
 
-export interface Measure {
-    id?: string;
+export interface MeasureRequestDTO {
     customer_code: string;
     measure_datetime: Date;
     measure_type: string;
     measure_value: number;
     confirmed_value: boolean;
-    image?: string;
+    image: string;
     createdAt?: Date;
     updatedAt?: Date;
+}
+
+export interface Measure {
+    id: string;
+    customer_code: string;
+    measure_datetime: Date;
+    measure_type: string;
+    measure_value: number;
+    confirmed_value: boolean;
+    image: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface IResponseDTO {
+    image_url: string;
+    measure_value: string;
+    measure_uuid: string;
+}
+
+export interface IRequestDTO{
+    image: string;
+    customer_code: string;
+    measure_datetime: string;
+    measure_type: 'WATER' | 'GAS';
 }
 
 export interface ICustomer {
@@ -33,11 +57,13 @@ export interface ICustomer {
     save(newCustomer: CustomerRequest): Promise<Customer>;
     update(customer: Customer): Promise<Customer>;
     getById(id: string): Promise<Customer>;
+    getByEmail(email: string): Promise<Customer>;
 }
 
 export interface IMeasure {
-    save(newMeasure: Measure): Promise<Measure>;
+    save(newMeasure: MeasureRequestDTO): Promise<Measure>;
     update(measure: Measure): Promise<Measure>;
     getById(id: string): Promise<Measure>;
     getByCustomerId(customerId: string): Promise<Measure[]>;
+    getByData(customerId: string, measureDatetime: string): Promise<Measure>;
 }
