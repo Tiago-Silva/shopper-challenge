36b9eca708d8c826d01f6c2a49fd23b1e1f065d1
Tiago <ti.almeida58@gmail.com>
2024-08-30 21:51:55 -0300

    feat: creating get measure list and patch confirm

--- server/service/measureService.ts
+++ server/service/measureService.ts
@@ -2,7 +2,7 @@ import prisma from "../config/prismaConfig";
 import fs from 'fs';
 import path from 'path';
 import dotenv from "dotenv";
-import {IRequestDTO, IResponseDTO, Measure} from "../interface/interface";
+import {IMeasureListDTO, IPathRequestDTO, IRequestDTO, IResponseDTO, Measure} from "../interface/interface";
 import {GenerateContentResult, GoogleGenerativeAI} from "@google/generative-ai";
 import {MeasureRepository} from "../repository/measureRepository";
 dotenv.config();
@@ -14,7 +14,7 @@ const model = configuration.getGenerativeModel({ model: modelId });
 export class MeasureService {
     private measureRepository = new MeasureRepository(prisma);
 
-    async uploadImage(data: IRequestDTO): Promise<IResponseDTO | { error_code: string, error_description: string }> {
+    async upload(data: IRequestDTO): Promise<IResponseDTO | { error_code: string, error_description: string }> {
         const { image, customer_code, measure_datetime, measure_type } = data;
         if (!image || !customer_code || !measure_datetime || !measure_type) {
             return {
@@ -35,26 +35,113 @@ export class MeasureService {
         try {
             const result = await this.getGeminiAI(base64Data);
             const filePath = this.saveImage(base64Data, customer_code);
-            const measure = await this.saveMeasure(data, parseInt(result.response.text()), false);
+            const measure = await this.saveMeasure(data, parseInt(result.response.text()), false, filePath);
 
             return {
                 image_url: filePath,
                 measure_value: measure.measure_value.toString(),
-                measure_uuid: measure.id
+                measure_uuid: measure.measure_uuid
             };
         } catch (e) {
             throw new Error('Error processing image');
         }
     }
 
-    async saveMeasure(requestDTO: IRequestDTO, measureValue: number, confirmed: boolean): Promise<Measure> {
+    async confirm(data: IPathRequestDTO): Promise<{ success: boolean } | { error_code: string, error_description: string }> {
+        const { measure_uuid, confirmed_value } = data;
+        if (!measure_uuid || !confirmed_value) {
+            return {
+                error_code: "INVALID_DATA",
+                error_description: "Invalid data"
+            };
+        }
+
+        const measure = await this.measureRepository.getById(measure_uuid);
+        if (!measure) {
+            return {
+                error_code: "MEASURE_NOT_FOUND",
+                error_description: "Leitura não encontrada"
+            }
+        }
+
+        if (this.checkDuplication(measure.measure_datetime) && measure.has_confirmed) {
+            return {
+                error_code: "CONFIRMATION_DUPLICATE",
+                error_description: "Leitura do mês já realizada"
+            };
+        }
+
+        measure.has_confirmed = true;
+        const update = await this.measureRepository.update(measure);
+
+        return {success: update.has_confirmed};
+    }
+
+    async getMeasureList(customerId: string, measureType: string | null): Promise<IMeasureListDTO | { error_code: string, error_description: string }> {
+        let measures;
+        if (measureType && measureType.trim() !== '') {
+            const validMeasureType = ['WATER', 'GAS'];
+            const measureTypeUpper = (measureType as string).toUpperCase();
+            if (!validMeasureType.includes(measureTypeUpper)) {
+                return {
+                    error_code: "INVALID_TYPE",
+                    error_description: "Tipo de medição não permitida"
+                }
+            }
+            measures = await this.getByCustomerIdAndType(customerId, measureTypeUpper);
+        } else {
+            measures = await this.getByCustomerId(customerId);
+        }
+
+        if (measures.length <= 0) {
+            return {
+                error_code: "MEASURES_NOT_FOUND",
+                error_description: "Nenhuma leitura encontrada"
+            }
+        }
+
+        return {
+            customer_code: customerId,
+            measures: measures
+        };
+    }
+
+    async getByCustomerId(customerId: string): Promise<Measure[]> {
+        try {
+            return this.measureRepository.getByCustomerId(customerId);
+        } catch (e) {
+            throw new Error('Error get measures by customer id');
+        }
+    }
+
+    async getByCustomerIdAndType(customerId: string, measureType: string): Promise<Measure[]> {
+        try {
+            return this.measureRepository.getByIdAndType(customerId, measureType);
+        } catch (e) {
+            throw new Error('Error get measures by customer id and type');
+        }
+    }
+
+    checkDuplication(measureDateTime: Date): boolean {
+        const currentDate = new Date();
+        const currentMonth = currentDate.getMonth();
+        const currentYear = currentDate.getFullYear();
+
+        const measureDate = new Date(measureDateTime);
+        const measureMonth = measureDate.getMonth();
+        const measureYear = measureDate.getFullYear();
+
+        return measureMonth === currentMonth && measureYear === currentYear;
+    }
+
+    async saveMeasure(requestDTO: IRequestDTO, measureValue: number, confirmed: boolean, image_url: string): Promise<Measure> {
         return this.measureRepository.save({
             customer_code: requestDTO.customer_code,
             measure_datetime: new Date(),
             measure_type: requestDTO.measure_type,
             measure_value: measureValue,
-            confirmed_value: confirmed,
-            image: requestDTO.image
+            has_confirmed: confirmed,
+            image_url: image_url
         });
     }
 
