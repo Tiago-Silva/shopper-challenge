98c60e4069c3eee8b89dba4601bc51ab9794a9fb
Tiago <ti.almeida58@gmail.com>
2024-09-05 08:54:36 -0300

    feat: creating measure.repository.prisma.ts

--- server/infrastructure/repository/measure.repository.prisma.ts
+++ server/infrastructure/repository/measure.repository.prisma.ts
@@ -1,18 +1,20 @@
-import { PrismaClient } from '@prisma/client'
-import {IMeasure, Measure, MeasureRequestDTO} from "../../interface/interface";
+import { PrismaClient, Measure as PrismaMeasure } from '@prisma/client';
+import { MeasureGateway } from "../../application/gateway/MeasureGateway";
+import { MeasureRequestDTO } from "../DTO/MeasureRequestDTO";
+import { Measure } from "../../domain/entity/Measure";
 
-
-export class MeasureRepositoryPrisma implements IMeasure {
+export class MeasureRepositoryPrisma implements MeasureGateway {
     constructor(private readonly prisma: PrismaClient) {
-        this.prisma = prisma
+        this.prisma = prisma;
     }
 
     async save(newMeasure: MeasureRequestDTO): Promise<Measure> {
-        return this.prisma.measure.create({data: newMeasure});
+        const createdMeasure: PrismaMeasure = await this.prisma.measure.create({ data: newMeasure });
+        return Measure.fromPrismaToMeasure(createdMeasure);
     }
 
     async update(measure: Measure): Promise<Measure> {
-        return this.prisma.measure.update({
+        const updatedMeasure: PrismaMeasure = await this.prisma.measure.update({
             where: {
                 measure_uuid: measure.measure_uuid
             },
@@ -21,37 +23,40 @@ export class MeasureRepositoryPrisma implements IMeasure {
                 measure_datetime: measure.measure_datetime,
                 measure_type: measure.measure_type,
                 measure_value: measure.measure_value,
-                has_confirmed: measure.has_confirmed,
+                has_confirmed: measure.has_confirmed as boolean,
                 image_url: measure.image_url,
                 createdAt: measure.createdAt,
                 updatedAt: measure.updatedAt
             }
         });
+        return Measure.fromPrismaToMeasure(updatedMeasure);
     }
 
     async getById(id: string): Promise<Measure> {
-        const measure = await this.prisma.measure.findUnique(({
+        const measure: PrismaMeasure | null = await this.prisma.measure.findUnique({
             where: { measure_uuid: id }
-        }));
+        });
 
         if (!measure) {
             throw new Error('Measure not found');
         }
 
-        return measure as Measure;
+        return Measure.fromPrismaToMeasure(measure);
     }
 
     async getByCustomerId(customerId: string): Promise<Measure[]> {
-        return this.prisma.measure.findMany({
+        const measures: PrismaMeasure[] = await this.prisma.measure.findMany({
             where: { customer_code: customerId }
         });
+
+        return measures.map(Measure.fromPrismaToMeasure);
     }
 
     async getByData(customerId: string, measureDatetime: string): Promise<Measure> {
         const date = new Date(measureDatetime);
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
-        const measure = await this.prisma.measure.findFirst({
+        const measure: PrismaMeasure | null = await this.prisma.measure.findFirst({
             where: {
                 customer_code: customerId,
                 AND: [
@@ -69,15 +74,17 @@ export class MeasureRepositoryPrisma implements IMeasure {
             throw new Error('Measure not found');
         }
 
-        return measure;
+        return Measure.fromPrismaToMeasure(measure);
     }
 
     async getByIdAndType(customerId: string, measureType: string): Promise<Measure[]> {
-        return this.prisma.measure.findMany({
+        const measures: PrismaMeasure[] = await this.prisma.measure.findMany({
             where: {
                 customer_code: customerId,
                 measure_type: measureType,
             }
         });
+
+        return measures.map(Measure.fromPrismaToMeasure);
     }
 }
