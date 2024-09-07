eb7a2ffba7297be2e4379b6ba98085c1c182606b
Tiago <ti.almeida58@gmail.com>
2024-09-07 20:51:04 -0300

    refactor: adding upload method

--- server/application/gateway/MeasureGateway.ts
+++ server/application/gateway/MeasureGateway.ts
@@ -1,8 +1,10 @@
 import {Measure} from "../../domain/entity/Measure";
+import {UploadInputData, UploadOutputData} from "../usecases/measure/upload-image-measure.usecase";
 
 export interface MeasureGateway {
     save(newMeasure: Measure): Promise<Measure>;
     update(measure: Measure): Promise<Measure>;
+    upload(input: UploadInputData): Promise<UploadOutputData>;
     getById(id: string): Promise<Measure>;
     getByCustomerId(customerId: string): Promise<Measure[]>;
     getByData(customerId: string, measureDatetime: string): Promise<Measure>;
