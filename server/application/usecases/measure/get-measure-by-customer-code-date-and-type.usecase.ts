28aeca49f062bec09edc894dfcc91f4311f622f4
Tiago <ti.almeida58@gmail.com>
2024-09-09 21:33:14 -0300

    refactor: rename

--- server/application/usecases/measure/get-measure-by-customer-code-date-and-type.usecase.ts
+++ server/application/usecases/measure/get-measure-by-customer-code-date-and-type.usecase.ts
@@ -2,17 +2,17 @@ import {Usecase} from "../usecase";
 import {Measure} from "../../../domain/entity/Measure";
 import {MeasureGateway} from "../../gateway/MeasureGateway";
 
-export class GetMeasureBydateandtypeUsecase implements Usecase<Measure, Measure> {
+export class GetMeasureByCustomerCodeDateAndTypeUsecase implements Usecase<Measure, Measure> {
 
     private constructor(private readonly measureGateway: MeasureGateway) {};
 
     public static create(measureGateway: MeasureGateway) {
-        return new GetMeasureBydateandtypeUsecase(measureGateway);
+        return new GetMeasureByCustomerCodeDateAndTypeUsecase(measureGateway);
     }
 
     public async exec(input: Measure): Promise<Measure> {
         const {customer_code, measure_datetime, measure_type} = input;
-        const existingMeasure =  await this.measureGateway.getByDateAndType(customer_code, measure_datetime.toString(), measure_type);
+        const existingMeasure =  await this.measureGateway.getByCustomerCodeDateAndType(customer_code, measure_datetime.toString(), measure_type);
         if (existingMeasure) {
             throw new Error('DOUBLE_REPORT');
         }
